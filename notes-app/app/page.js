e1483634f65cf5b8b6188a70e931deb434b16b5b
rishabh392004 <rishabhguptkd@gmail.com>
2025-12-29 21:20:21 +0530

    save

--- notes-app/app/page.js
+++ notes-app/app/page.js
@@ -1,7 +1,20 @@
 import dbConnect from "@/lib/db";
 import NotesClient from "@/components/NotesClient"
+import Note from "@/models/Note"
+import { Noto_Serif_Display } from "next/font/google";
+async function getNotes(){
+    await dbConnect();
+  const notes =await Note.find({}).sort({ createdAt: -1 });
+  return notes.map((note)=>({
+    ...note,
+    _id:note._id.toString()
+  }))
+
+}
 export default  async function Home() {
-  await dbConnect();
+  const notes=await getNotes()
+console.log(notes)
+
   return(
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6cd ">Notes App</h1>
--- notes-app/models/Note.js
+++ notes-app/models/Note.js
@@ -1,26 +1,28 @@
 import mongoose from "mongoose";
-const NoteSchema=new mongoose.Schema({
-title:{
-    type:String,
-    required:true,
-    maxLength:100
-},
-content:{
-    type:String,
-    required:true,
-    maxLength:2000
-},
-createdAt:{
-    type:Date,
-    default:Date.now
-},
-updatedAt:{
-    type:Date,
-    default:Date.now
-}
-})
-NoteSchema.pre("save",function(next){
-    this.updatedAt=Date.now()
-    next()
-})
+
+const NoteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  content: {
+    type: String,
+    required: true,
+    maxLength: 2000,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+NoteSchema.pre("save", function () {
+  this.updatedAt = Date.now();
+});
+
 export default mongoose.models.Note || mongoose.model("Note", NoteSchema);
